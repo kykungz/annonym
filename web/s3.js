418218c06d6509ed641748cff6f103ee2869da25
Kongpon <jackykongpon@gmail.com>
2020-05-30 21:06:21 +0700

    Remove unused code

--- web/s3.js
+++ web/s3.js
@@ -10,37 +10,3 @@ module.exports.s3 = new AWS.S3({
     Bucket: 'annonym-bucket',
   },
 })
-
-/* getGetSignedUrl generates an aws signed url to retreive an item
- * @Params
- *    key: string - the filename to be put into the s3 bucket
- * @Returns:
- *    a url as a string
- */
-module.exports.getGetSignedUrl = (key) => {
-  const signedUrlExpireSeconds = 60 * 5
-
-  const url = s3.getSignedUrl('getObject', {
-    Key: key,
-    Expires: signedUrlExpireSeconds,
-  })
-
-  return url
-}
-
-/* getPutSignedUrl generates an aws signed url to put an item
- * @Params
- *    key: string - the filename to be retreived from s3 bucket
- * @Returns:
- *    a url as a string
- */
-module.exports.getPutSignedUrl = (key) => {
-  const signedUrlExpireSeconds = 60 * 3
-
-  const url = s3.getSignedUrl('putObject', {
-    Key: key,
-    Expires: signedUrlExpireSeconds,
-  })
-
-  return url
-}
